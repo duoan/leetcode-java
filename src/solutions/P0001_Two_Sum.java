b37dd5031381e3987f6a03b28456b3c5f21b0b13
Victor An <anduo@qq.com>
2022-12-15 08:21:21 +0000

    update two sum tech interview problem clarification questions

--- src/solutions/P0001_Two_Sum.java
+++ src/solutions/P0001_Two_Sum.java
@@ -5,6 +5,27 @@ import java.util.HashMap;
 
 // 两数之和（LeetCode 1）:https://leetcode-cn.com/problems/two-sum/
 
+// clarify the problems:
+// about the input#1, nums
+// q1: is the input a integer array ?
+// q2: how about the the size of the array?
+// q3: is the arrray sorted?
+// about the input#2, target
+// q4: how abou the target range, an integer or long
+// about the output
+// q5: does only one valid answer exist?
+// q6: if not exist one valid answer, can I return the {-1, -1}, since -1 is invalid index in array
+// q7: can I use the same element twice, like nums:[5, 1], target: 5, return [0, 0]
+
+// solutions:
+// 1. brute forch: 2 loops
+//.   time:  O(n^2)
+//.   space: O(1)
+// 2. sorting the nums, then using 2 pointer [left from 0 to right, right from end to left]
+//.   time:  O(nlogn), // merge sort worse case, O(nlogn) quick sort average case 
+//.   space: O(n) // merge sort, O(logn) quick sort
+// 3. hashmap to cache the seen num:index [key: num, value: index], lookup the target - nums[i] in the map, if found return the {map[target-nums[i]], i}
+//.   O(n), O(n)
 public class P0001_Two_Sum {
 
     public int[] twoSum(int[] nums, int target) {
